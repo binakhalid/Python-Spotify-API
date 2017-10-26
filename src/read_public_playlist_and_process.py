2da76b88d9dae3f487dfd24546a15a71bd049a87
Bina Khalid <binaanwar@gmail.com>
2017-10-26 15:01:13 +0300

    first commit

---
+++ src/read_public_playlist_and_process.py
@@ -0,0 +1,62 @@
+# import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+#import json
+
+clientId = "bddce1a6d99d41fc852233c8501f8e36"
+clientSecret = "633a19aeeac7453cbaca0b81412e4d35"
+spotify_user_name_first_playlist = "spotify"
+spotify_user_name_second_playlist = "onerepublicofficial"
+first_playlist_id = "37i9dQZF1DXbZBKpgxsyXU"
+second_playlist_id = "0V4WND9P7IDEdtZjOJomHi"
+track_list_1 = []
+track_list_2 = []
+
+client_credentials_manager = SpotifyClientCredentials(clientId, clientSecret)
+spotify_manager = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_tracks_for_first_playlist(first_playlist_id):
+    results = spotify_manager.user_playlist(spotify_user_name_first_playlist,
+                                                   first_playlist_id,
+                                                   fields="tracks,next")
+    tracks = results['tracks']
+    for item in tracks['items']:
+        track = item['track']
+        track_list_1.append(track['name'])
+    return track_list_1
+
+def get_tracks_for_second_playlist(second_playlist_id):
+    results = spotify_manager.user_playlist(spotify_user_name_second_playlist,
+                                                   second_playlist_id,
+                                                   fields="tracks,next")
+    tracks = results['tracks']
+    for item in tracks['items']:
+        track = item['track']
+        track_list_2.append(track['name'])
+    return track_list_2
+
+track_list_1 = get_tracks_for_first_playlist(first_playlist_id)
+print('Track List # 1')
+print('---------------')
+print(track_list_1)
+track_list_2 = get_tracks_for_second_playlist(second_playlist_id)
+print('Track List # 2')
+print('---------------')
+print(track_list_2)
+print('=======================================================')
+
+def compare_lists_and_generate_common_name_list(track_list_1, track_list_2):
+    common_name_list = list(set(track_list_1).intersection(track_list_2))
+    print('Track List Common Track Names')
+    print('-----------------------------')
+    print(common_name_list)
+
+def combine_track_names_and_generate_list(track_list_1, track_list_2):
+    combined_list_names = list(track_list_1)
+    combined_list_names.extend(x for x in track_list_2 if x not in combined_list_names)
+    print('Track List Combined Track Names')
+    print('-------------------------------')
+    print(combined_list_names)
+
+compare_lists_and_generate_common_name_list(track_list_1, track_list_2)
+combine_track_names_and_generate_list(track_list_1, track_list_2)
